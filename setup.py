fc98db855019c8d99afe1879aa185b57ff59c450
Hartmut Goebel <h.goebel@goebel-consult.de>
2008-10-13 15:50:56 +0000

    Updating docs for release 0.4

--- setup.py
+++ setup.py
@@ -6,29 +6,7 @@ import ez_setup
 ez_setup.use_setuptools()
 from setuptools import setup
 
-description = "MANAGESIEVE client library for remotely managing Sieve scripts"
-long_description = """
-A MANGAGESIEVE client library for remotely managing Sieve scripts,
-including an interactive 'sieveshell'.
-
-This module allows accessing a Sieve-Server for managing Sieve scripts
-there. For more information about the MANAGESIEVE protocol see draft
-http://www.ietf.org/internet-drafts/draft-martin-managesieve-04.txt .
-
-
-What is MANAGESIEVE?
----------------------
-
-Sieve scripts allow users to filter incoming email. Message stores are
-commonly sealed servers so users cannot log into them, yet users must
-be able to update their scripts on them. This module implements a
-protocol "managesieve" for securely managing Sieve scripts on a remote
-server. This protocol allows a user to have multiple scripts, and also
-alerts a user to syntactically flawed scripts.
-
-This an interim measure as it is hoped that eventually Sieve scripts
-will be stored on ACAP.
-""" # " emacs happy
+description = "ManageSieve client library for remotely managing Sieve scripts"
 
 from distutils.command.bdist_rpm import bdist_rpm
 
@@ -59,7 +37,7 @@ class MyBDist_RPM(bdist_rpm):
 setup (name = "managesieve",
        version = "0.4",
        description = description,
-       long_description = long_description,
+       long_description = open('README').read().strip(),
        author = "Hartmut Goebel",
        author_email = "h.goebel@crazy-compilers.com",
        #maintainer = "Hartmut Goebel",
