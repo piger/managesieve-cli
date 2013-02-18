3ec934c903d7faecdf77b52fa0241fe6b85b3b5d
sand <daniel@spatof.org>
2013-02-18 15:11:46 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ class MyBDist_RPM(bdist_rpm):
 
 setup(
     name = "managesieve",
-    version = "0.4.3-dev",
+    version = "0.4.4-dev",
     description = description,
     long_description = open('README.txt').read().strip(),
     author = "Daniel Kertesz",
